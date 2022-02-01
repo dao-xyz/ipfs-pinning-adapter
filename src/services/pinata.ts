a601b06c11d19b4cbff2801d0746e33afa346537
Marcus Pousette <marcus.pousette@quantleaf.com>
2022-02-01 09:21:03 +0100

    Remove comment

--- src/services/pinata.ts
+++ src/services/pinata.ts
@@ -9,21 +9,18 @@ import { generateUUID } from "../utils";
  * @returns pinner generator
  */
 export const getPinner = (config: KeySecretConfig): Pinner => {
-  const pinataSDK = require("@pinata/sdk");
-  const pinata = pinataSDK(config.apiKey, config.secret);
-  return async (buffer: Buffer) => {
-    /*   const arrayBuffer = await blob.arrayBuffer();
-          var view = new Uint8Array(arrayBuffer);
-          var newBuffer = Buffer.from(view); */
-    const stream = Readable.from(buffer);
-    const fileName = new Date().getTime() + "_" + generateUUID(); // Filename is required by Pinata
-    (stream as any).path = fileName;
-    return pinata
-      .pinFileToIPFS(stream)
-      .then(
-        (result: { IpfsHash: string; PinSize: string; Timestamp: string }) => {
-          return result.IpfsHash;
-        }
-      );
-  };
+    const pinataSDK = require("@pinata/sdk");
+    const pinata = pinataSDK(config.apiKey, config.secret);
+    return async (buffer: Buffer) => {
+        const stream = Readable.from(buffer);
+        const fileName = new Date().getTime() + "_" + generateUUID(); // Filename is required by Pinata
+        (stream as any).path = fileName;
+        return pinata
+            .pinFileToIPFS(stream)
+            .then(
+                (result: { IpfsHash: string; PinSize: string; Timestamp: string }) => {
+                    return result.IpfsHash;
+                }
+            );
+    };
 };
